6431d8f26a749fa9d01be2e797b071983d71bf95
Jonathan Dabre <dabrejonathan.manuel@wissen.com>
2025-04-03 16:58:03 +0530

    configure Vite server to allow external access and set port

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // 👈 Allows Vite to be accessed from outside the container
+    port: 5173,       // 👈 Make sure it matches the exposed port
+  }
 })
